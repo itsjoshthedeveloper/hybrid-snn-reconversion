dfb7bfb348ebd681a49b95a639353ff8968f61d4
Josh Chough <joshchough@gmail.com>
2020-08-01 16:52:59 -0400

    Attack ANN and ANN' with FGSM and graph plot

--- snn_to_ann.py
+++ snn_to_ann.py
@@ -42,6 +42,80 @@ class AverageMeter(object):
         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
         return fmtstr.format(**self.__dict__)
 
+# FGSM attack code
+def fgsm_attack(image, epsilon, data_grad):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image
+    perturbed_image = image + epsilon*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+def testFGSM( test_loader, model, device, epsilon ):
+
+    # Accuracy counter
+    correct = 0
+    adv_examples = []
+    model.eval()
+
+    # Loop over all examples in test set
+    for batch_idx, (data, target) in enumerate(test_loader):
+
+        # # Send the data and label to the device
+        # data, target = data.to(device), target.to(device)
+
+        # Set requires_grad attribute of tensor. Important for Attack
+        data.requires_grad = True
+
+        # Forward pass the data through the model
+        output = model(data)
+        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+
+        # If the initial prediction is wrong, dont bother attacking, just move on
+        if init_pred.item() != target.item():
+            continue
+
+        # Calculate the loss
+        loss = F.nll_loss(output, target)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect datagrad
+        data_grad = data.grad.data
+
+        # Call FGSM Attack
+        perturbed_data = fgsm_attack(data, epsilon, data_grad)
+
+        # Re-classify the perturbed image
+        output = model(perturbed_data)
+
+        # Check for success
+        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        if final_pred.item() == target.item():
+            correct += 1
+            # Special case for saving 0 epsilon examples
+            if (epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+        else:
+            # Save some adv examples for visualization later
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+
+    # Calculate final accuracy for this epsilon
+    final_acc = correct/float(len(test_loader))
+    f.write("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+
+    # Return the accuracy and an adversarial example
+    return final_acc, adv_examples
+
 def test(loader, model, identifier):
     losses = AverageMeter('Loss')
     top1   = AverageMeter('Acc@1')
@@ -94,12 +168,6 @@ def test(loader, model, identifier):
         filename = './trained_models/snn_to_ann/'+identifier+'.pth'
         torch.save(state, filename)
         
-        f.write('\n ' + identifier + '\n\ttest_loss: {:.4f}, test_acc: {:.4f}, time: {}'.  format(
-            state['test_loss'], 
-            state['test_acc'],
-            state['time']
-            )
-        )
         # f.write(' test_loss: {:.4f}, test_acc: {:.4f}, best: {:.4f}, time: {}'.  format(
         #     losses.avg, 
         #     top1.avg,
@@ -112,7 +180,7 @@ def test(loader, model, identifier):
         #     )
         # )
 
-        return top1.avg
+        return state
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Reconvert SNN to ANN', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -252,6 +320,7 @@ if __name__ == '__main__':
     
     # train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    test_loader_fgsm = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=True)
     
     if architecture[0:3].lower() == 'vgg':
         model = VGG(vgg_name=architecture, labels=labels, dataset=dataset, kernel_size=kernel_size, dropout=dropout)
@@ -298,11 +367,11 @@ if __name__ == '__main__':
                 else:
                     f.write('\n Error [ANN\']: Size mismatch at {}, size of loaded model {}, size of current model {}'.format(key, state['state_dict'][key].shape, model_prime.state_dict()[key].shape))
             else:
-                f.write('\n Error [ANN\']: Loaded weight {} not present in current model'.format(state['state_dict'][key]))
+                f.write('\n Error [ANN\']: Loaded weight {} not present in current model'.format(key))
         
         model_prime.load_state_dict(cur_dict)
     
-    f.write('\n {}'.format(model_prime)) 
+    f.write('\n {}'.format(model_prime) + '\n')
     
     # if torch.cuda.is_available() and args.gpu:
     #     model.cuda()
@@ -319,10 +388,56 @@ if __name__ == '__main__':
     #     test(epoch)
 
     start_time = datetime.datetime.now()
-    ann_accuracy = test(test_loader, model, ann_identifier)
-
+    ann_state = test(test_loader, model, ann_identifier)
     start_time = datetime.datetime.now()
-    ann_prime_accuracy = test(test_loader, model_prime, ann_prime_identifier)
+    ann_prime_state = test(test_loader, model_prime, ann_prime_identifier)
+
+    epsilons = [0, .05, .1, .15, .2, .25, .3]
+    f.write('\n epsilons: {}'.format(epsilons))
+    device = torch.device("cuda" if (args.gpu and torch.cuda.is_available()) else "cpu")
+    ann_accuracies_fgsm = []
+    ann_examples_fgsm = []
+    ann_prime_accuracies_fgsm = []
+    ann_prime_examples_fgsm = []
+
+    # Run test for each epsilon
+    for eps in epsilons:
+        acc, ex = testFGSM(test_loader_fgsm, model, device, eps)
+        ann_accuracies_fgsm.append(acc)
+        ann_examples_fgsm.append(ex)
+
+        acc, ex = testFGSM(test_loader_fgsm, model_prime, device, eps)
+        ann_prime_accuracies_fgsm.append(acc)
+        ann_prime_examples_fgsm.append(ex)
+
+    f.write('\n\n ' + ann_identifier)
+    f.write('\n\ttest_loss: {:.4f}, test_acc: {:.4f}, time: {}'.format(
+            ann_state['test_loss'], 
+            ann_state['test_acc'],
+            ann_state['time']
+            )
+        )
+    f.write('\n\tfgsm accuracies: ' + ann_accuracies_fgsm)
+
+    f.write('\n\n ' + ann_prime_identifier)
+    f.write('\n\ttest_loss: {:.4f}, test_acc: {:.4f}, time: {}'.format(
+            ann_prime_state['test_loss'], 
+            ann_prime_state['test_acc'],
+            ann_prime_state['time']
+            )
+        )
+    f.write('\n\tfgsm accuracies: ' + ann_prime_accuracies_fgsm)
+    
+    plt.figure(figsize=(5,5))
+    plt.plot(epsilons, ann_accuracies_fgsm, label='ANN')
+    plt.plot(epsilons, ann_prime_accuracies_fgsm, label='ANN\'')
+    plt.yticks(np.arange(0, 1.1, step=0.1))
+    plt.xticks(np.arange(0, .35, step=0.05))
+    plt.title("Accuracy vs Epsilon [{}]".format(ann_identifier))
+    plt.xlabel("Epsilon")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.savefig('./logs/snn_to_ann/'+ann_identifier+'.png', bbox_inches='tight')
 
     # f.write('\n ANN accuracy: {:.4f}'.format(ann_accuracy))
     # f.write('\n ANN\' accuracy: {:.4f}'.format(ann_prime_accuracy))
