1bc5006df209b78482e01df08275515cdd9c27e9
Josh Chough <joshchough@gmail.com>
2020-08-25 01:08:47 -0400

    Created shell scripts for each run

---
+++ job_snn_30.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -p pi_panda --gres=gpu:1 --mem=20G -t 40:00:00 --mail-user=josh.chough@yale.edu --mail-type=ALL
+
+module load miniconda
+source activate pytorch_env
+python snn.py --epochs 30 --lr_reduce 5 --batch_size 32 --pretrained_ann './trained_models/ann/ann_vgg16_cifar10.pth' --log
