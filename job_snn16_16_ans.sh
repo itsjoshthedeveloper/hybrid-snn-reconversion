a53883a81a9ad431c6545d91343cb2a6e857936b
Josh Chough <joshchough@gmail.com>
2020-09-02 14:55:56 -0400

    New shell scripts for new jobs

---
+++ job_snn16_16_ans.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -p pi_panda --gres=gpu:1 --mem=20G -t 40:00:00 --mail-user=josh.chough@yale.edu --mail-type=ALL
+
+module load miniconda
+source activate pytorch_env
+python snn_ans.py --epochs 16 --lr_reduce 5 --batch_size 32 --pretrained_ann './trained_models/snn_ans/ann_vgg16_cifar10.pth' --log --ans True
